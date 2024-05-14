90c034bc422e8f26ded818713fe479d9d177264a
somshekharbirajdar21 <shekharbirajdar666@outlook.com>
2024-05-14 20:39:42 +0530

    Starrted Creating SignIn API

--- djangoauth/UserSignIn/UserSerializers.py
+++ djangoauth/UserSignIn/UserSerializers.py
@@ -1,1 +1,13 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
+
+class UserSignInSerializer(serializers.Serializer):
+    mobileNumber = serializers.CharField(max_length = 10)
+    class Meta:
+        model = User
+        fields = ['id','email','username','password', 'mobileNumber']
+
+    def save(self,validated_data):
+        data = User.set_password(validated_data['password'])
+        data = User.objects.create(validated_data['email'],validated_data['username'],validated_data['mobileNumber'])
+        data.save()
--- djangoauth/UserSignIn/views.py
+++ djangoauth/UserSignIn/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .UserSerializers import UserSignInSerializer
 
 # Create your views here.
 @api_view(['POST'])
 def signInView(request):
-    return Response("hello")
+    serializer = UserSignInSerializer(data = request.data)
+    if serializer.is_valid():
+        return Response(serializer.data)
+    else:
+        return Response(serializer.errors)
